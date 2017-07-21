808d3c0362b397547832a8fbc3c9ea4a9bbfd35c
WeiTsungCheng <sanctuary85c@gmail.com>
2017-07-21 12:33:02 +0800

    midtest-1

---
+++ midTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  midTest
+//
+//  Created by Wei-Tsung Cheng on 2017/7/21.
+//  Copyright © 2017年 Wei-Tsung Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
