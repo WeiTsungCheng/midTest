5eba1c53719a8aa00fb2ad15d93269919b260d06
WeiTsungCheng <sanctuary85c@gmail.com>
2017-07-23 00:19:30 +0800

    good practice

--- midTest/ViewController.swift
+++ midTest/ViewController.swift
@@ -28,39 +28,57 @@ UINavigationControllerDelegate, UIScrollViewDelegate {
 
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
             let imagePicker = UIImagePickerController()
+            //UIImagePickerController指派給自己(ViewController)
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.camera
             imagePicker.allowsEditing = false
             self.present(imagePicker, animated: true, completion: nil)
+
         }
-
-    
-
     }
 
 
     //選照片
-    @IBAction func toGetPicture(_ sender: Any) {
+    @IBAction func toGetPicture(_ sender: Any)
+    {
 
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
 
-            let imagePicker = UIImagePickerController()
 
-                imagePicker.delegate = self
+            let imagePicker = UIImagePickerController()
+            //UIImagePickerController指派給自己(ViewController)
+            imagePicker.delegate = self
 
                 imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
 
                 imagePicker.allowsEditing = true
 
                 self.present(imagePicker, animated: true, completion: nil)
+               // photo.contentMode = .scaleAspectFit
 
             }
 
     }
 
+    //存檔
+    @IBAction func savePhoto(_ sender: Any) {
+
+        let imageData = UIImageJPEGRepresentation(photo.image!, 1)
+
+        let compressedJPGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPGImage!, nil, nil, nil)
 
 
-////放照片
+        let alert = UIAlertController(title: "Wow", message: "Your image has been saved to Photo Library!", preferredStyle: UIAlertControllerStyle.alert)
+
+        let callaction = UIAlertAction(title: "OK", style: .default , handler:nil)
+
+        alert.addAction(callaction)
+
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    //放照片
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             photo.image = image
@@ -69,21 +87,24 @@ UINavigationControllerDelegate, UIScrollViewDelegate {
         }
         self.dismiss(animated: true, completion: nil)
 
-
+        //不要把placehold預設圖像放的填滿控制放在viewDidLoad()下，否則剛進入頁面時會造成預設圖像模糊，而是放在拿完圖後（拍照,或相簿拿圖）,存入imageView時再改成scaleAspectFit，讓圖片撐開外面的scrollView
+        photo.contentMode = UIViewContentMode.scaleAspectFit
 
     }
 
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photo
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
 
         showImage.delegate = self
         showImage.minimumZoomScale = 1.0
         showImage.maximumZoomScale = 10.0
 
         photo.tintColor = UIColor.white
-        photo.contentMode = .scaleAspectFill
         upView.backgroundColor = UIColor.asiBlack
         downView.backgroundColor = UIColor.asiDandelion
 
@@ -96,11 +117,6 @@ UINavigationControllerDelegate, UIScrollViewDelegate {
         toGetPicture.titleLabel?.font = UIFont.asiTextStyle23Font()
         toGetPicture.setTitle("Pick an Image", for: UIControlState.normal)
         toGetPicture.setTitleColor(UIColor.white, for: UIControlState.normal)
-
-    }
-
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return photo
 
     }
 
